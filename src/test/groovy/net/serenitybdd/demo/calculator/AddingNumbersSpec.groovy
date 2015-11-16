14f76552f8c05fe55914ac7ef14cf4f9ad2a7487
John Smart <john.smart@wakaleo.com>
2015-11-17 09:17:58 +1100

    Added dummy test

--- src/test/groovy/net/serenitybdd/demo/calculator/AddingNumbersSpec.groovy
+++ src/test/groovy/net/serenitybdd/demo/calculator/AddingNumbersSpec.groovy
@@ -4,6 +4,12 @@ import spock.lang.Specification
 class AddingNumbersSpec extends Specification {
 
     def "should add some numbers"() {
-        // TODO
+        given:
+            int a = 1
+            int b = 2
+        when:
+            def result = a + b
+        then:
+            result == 3
     }
 }
